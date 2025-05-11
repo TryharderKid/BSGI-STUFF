78e7894f4757bf08da6ddd56b79b34bbfc449031
IlIIlllllII <yozyyozy251@gmail.com>
2025-05-10 23:34:12 -0500

    Create Eggs.lua

---
+++ Eggs.lua
@@ -0,0 +1,233 @@
+local EggModule = {
+    Eggs = {
+        ["Common Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/5/5b/Common_Egg.png/revision/latest",
+            ["Pet Amount"] = 5,
+            ["Location"] = "World 1 Surface",
+            ["Currency"] = "Coins",
+            ["Amount"] = 10,
+            ["Obtainable"] = true
+        },
+        
+        ["Spotted Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/6/6f/Spotted_Egg.png/revision/latest",
+            ["Pet Amount"] = 5,
+            ["Location"] = "World 1 Surface",
+            ["Currency"] = "Coins",
+            ["Amount"] = 110,
+            ["Obtainable"] = true
+        },
+        
+        ["Iceshard Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/3/3e/Iceshard_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "World 1 Surface",
+            ["Currency"] = "Coins",
+            ["Amount"] = 450,
+            ["Obtainable"] = true
+        },
+        
+        ["Spikey Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/9/93/Spikey_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "Floating Island",
+            ["Currency"] = "Coins",
+            ["Amount"] = 5000,
+            ["Obtainable"] = true
+        },
+        
+        ["Magma Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/6/63/Magma_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "Outer Space",
+            ["Currency"] = "Coins",
+            ["Amount"] = 15000,
+            ["Obtainable"] = true
+        },
+        
+        ["Crystal Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/6/6d/Crystal_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "Outer Space",
+            ["Currency"] = "Coins",
+            ["Amount"] = 75000,
+            ["Obtainable"] = true
+        },
+        
+        ["Lunar Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/6/64/Lunar_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "Twilight",
+            ["Currency"] = "Coins",
+            ["Amount"] = 100000,
+            ["Obtainable"] = true
+        },
+        
+        ["Void Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/5/58/Void_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "The Void",
+            ["Currency"] = "Coins",
+            ["Amount"] = 175000,
+            ["Obtainable"] = true
+        },
+        
+        ["Hell Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/a/a2/Hell_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "The Void",
+            ["Currency"] = "Coins",
+            ["Amount"] = 300000,
+            ["Obtainable"] = true
+        },
+        
+        ["Nightmare Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/4/43/Nightmare_Egg.png/revision/latest",
+            ["Pet Amount"] = 7,
+            ["Location"] = "The Void",
+            ["Currency"] = "Coins",
+            ["Amount"] = 900000,
+            ["Obtainable"] = true
+        },
+        
+        ["Rainbow Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/3/3f/Rainbow_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "Zen",
+            ["Currency"] = "Coins",
+            ["Amount"] = 1500000,
+            ["Obtainable"] = true
+        },
+        
+        ["Infinity Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/2/24/Infinity_Egg.png/revision/latest",
+            ["Pet Amount"] = 85,
+            ["Location"] = "Hatching Zone",
+            ["Currency"] = "Coins",
+            ["Amount"] = 750000,
+            ["Obtainable"] = true
+        },
+        
+        ["Showman Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/1a/Showman_Egg.png/revision/latest",
+            ["Pet Amount"] = 3,
+            ["Location"] = "Dice Island",
+            ["Currency"] = "Tickets",
+            ["Amount"] = 1000,
+            ["Obtainable"] = true
+        },
+        
+        ["Mining Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/6/65/Mining_Egg.png/revision/latest",
+            ["Pet Amount"] = 6,
+            ["Location"] = "Minecart Forest",
+            ["Currency"] = "Tickets",
+            ["Amount"] = 120000,
+            ["Obtainable"] = true
+        },
+        
+        ["Cyber Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/7/7b/Cyber_Egg.png/revision/latest",
+            ["Pet Amount"] = 7,
+            ["Location"] = "Robot Factory",
+            ["Currency"] = "Tickets",
+            ["Amount"] = 450000,
+            ["Obtainable"] = true
+        },
+        
+        ["Game Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/9/98/Game_Egg.png/revision/latest",
+            ["Pet Amount"] = 4,
+            ["Location"] = "W2 Surface",
+            ["Currency"] = "Tickets",
+            ["Amount"] = 10000,
+            ["Obtainable"] = true
+        },
+        
+        ["Silly Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/3/3a/Silly_Egg.png/revision/latest",
+            ["Pet Amount"] = 5,
+            ["Location"] = "Rift Island",
+            ["Currency"] = "Coins",
+            ["Amount"] = 10000000,
+            ["Obtainable"] = true
+        },
+        
+        ["Season 1 Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/7/7c/Season_1_Egg.png/revision/latest",
+            ["Pet Amount"] = 4,
+            ["Location"] = "No Location",
+            ["Currency"] = "No Currency",
+            ["Amount"] = 0,
+            ["Obtainable"] = false,
+            ["ObtainMethod"] = "Bubble Pass"
+        },
+        
+        ["Season 2 Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/f/f9/Season_2_Egg.png/revision/latest",
+            ["Pet Amount"] = 4,
+            ["Location"] = "No Location",
+            ["Currency"] = "No Currency",
+            ["Amount"] = 0,
+            ["Obtainable"] = false,
+            ["ObtainMethod"] = "Bubble Pass"
+        },
+        
+        ["Series 1 Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/7/70/Series_1_Egg.png/revision/latest",
+            ["Pet Amount"] = 5,
+            ["Location"] = "No Location",
+            ["Currency"] = "No Currency",
+            ["Amount"] = 0,
+            ["Obtainable"] = false,
+            ["ObtainMethod"] = "Competitive Challenges"
+        },
+        
+        ["100M Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/5/55/100M_Egg.png/revision/latest",
+            ["Pet Amount"] = 7,
+            ["Location"] = "W1 Surface",
+            ["Currency"] = "Coins",
+            ["Amount"] = 1000000,
+            ["Obtainable"] = false
+        },
+        
+        ["Throwback Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/8/86/Throwback_Egg.png/revision/latest",
+            ["Pet Amount"] = 8,
+            ["Location"] = "Hatching Zone",
+            ["Currency"] = "Coins",
+            ["Amount"] = 1400000,
+            ["Obtainable"] = false
+        },
+        
+        ["Bunny Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/4/4d/Bunny_Egg.png/revision/latest",
+            ["Pet Amount"] = 7,
+            ["Location"] = "Easter Event 2025 Island",
+            ["Currency"] = "Coins",
+            ["Amount"] = 1200000,
+            ["Obtainable"] = false
+        },
+        
+        ["Pastel Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/c/cc/Pastel_Egg.png/revision/latest",
+            ["Pet Amount"] = 7,
+            ["Location"] = "Easter Event 2025 Island",
+            ["Currency"] = "Coins",
+            ["Amount"] = 800000,
+            ["Obtainable"] = false
+        },
+        
+        ["Aura Egg"] = {
+            ["ImageId"] = "https://static.wikia.nocookie.net/bgs-infinity/images/2/2e/Aura_Egg.png/revision/latest",
+            ["Pet Amount"] = 5,
+            ["Location"] = "Rift Island",
+            ["Currency"] = "Coins",
+            ["Amount"] = 7800000,
+            ["Obtainable"] = false
+        }
+    }
+}
+
+return EggModule
