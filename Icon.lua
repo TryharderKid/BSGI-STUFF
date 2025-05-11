6dea3599908307421bb926cfb0d14fd3db9ef537
IlIIlllllII <yozyyozy251@gmail.com>
2025-05-10 23:34:57 -0500

    Create  Icon.lua

---
+++ Icon.lua
@@ -0,0 +1,27 @@
+local GameIcons = {
+     Icons = {
+    Golden_Key = "https://static.wikia.nocookie.net/bgs-infinity/images/e/ec/Golden_Key.png/revision/latest/scale-to-width-down/80?cb=20250413155736",
+    Pet_Equips_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/2/28/Pet_Equips_Icon.png/revision/latest/scale-to-width-down/180?cb=20250430060716",
+    Bubbles = "https://static.wikia.nocookie.net/bgs-infinity/images/0/0c/Bubbles.png/revision/latest/scale-to-width-down/25?cb=20250430031457",
+    Coins = "https://static.wikia.nocookie.net/bgs-infinity/images/f/f0/Coins.png/revision/latest/scale-to-width-down/25?cb=20250430031523",
+    Tickets = "https://static.wikia.nocookie.net/bgs-infinity/images/1/14/Tickets.png/revision/latest/scale-to-width-down/25?cb=20250504225331",
+    Gems = "https://static.wikia.nocookie.net/bgs-infinity/images/d/d5/Gems.png/revision/latest/scale-to-width-down/25?cb=20250430031603",
+    Egg = "https://static.wikia.nocookie.net/bgs-infinity/images/2/26/Egg.png/revision/latest/scale-to-width-down/25?cb=20250503001541",
+    Robux_Shop_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/0/08/Robux_Shop_Icon.png/revision/latest/scale-to-width-down/25?cb=20250413015503",
+    Index_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/d/da/Index_Icon.png/revision/latest/scale-to-width-down/25?cb=20250502023202",
+    Quests_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/c/c2/Quests_Icon.png/revision/latest/scale-to-width-down/25?cb=20250413120133",
+    Giant_Chest_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/4/4a/Giant_Chest_Icon.png/revision/latest/scale-to-width-down/25?cb=20250418015401",
+    Wheel_Spin_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/5/54/Wheel_Spin_Icon.png/revision/latest/scale-to-width-down/25?cb=20250502074455",
+    Bubble_Pass_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/d/d5/Bubble_Pass_Icon.png/revision/latest/scale-to-width-down/25?cb=20250430013058",
+    Competitive_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/7/79/Competitive_Icon.png/revision/latest/scale-to-width-down/25?cb=20250504081145",
+    Trading_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/b/b3/Trading_Icon.png/revision/latest/scale-to-width-down/25?cb=20250502072511",
+    Dice_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/a/ad/Dice_Icon.png/revision/latest/scale-to-width-down/25?cb=20250506172603",
+    Common_Rarity = "https://static.wikia.nocookie.net/bgs-infinity/images/1/10/Common_Rarity.png/revision/latest/scale-to-width-down/180?cb=20250430055824",
+    Unique_Rarity = "https://static.wikia.nocookie.net/bgs-infinity/images/8/89/Unique_Rarity.png/revision/latest/scale-to-width-down/180?cb=20250430055904",
+    Rare_Rarity = "https://static.wikia.nocookie.net/bgs-infinity/images/9/90/Rare_Rarity.png/revision/latest/scale-to-width-down/180?cb=20250412204328",
+    Holy_Shock_stats = "https://static.wikia.nocookie.net/bgs-infinity/images/c/cc/Holy_Shock_stats.png/revision/latest/scale-to-width-down/180?cb=20250415035651",
+    Experienced_Icon = "https://static.wikia.nocookie.net/bgs-infinity/images/2/29/Experienced_Icon.png/revision/latest/scale-to-width-down/25?cb=20250415184911",
+    Logo = "https://static.wikia.nocookie.net/bgs-infinity/images/d/dc/Badge_-_Welcome.png/revision/latest/scale-to-width-down/125?cb=20250411092430",
+}
+
+return GameIcons
